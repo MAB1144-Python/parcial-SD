d39e6b69c7216d22d3c5df32496c3bd3c6bc41f2
MAB <mab1144@hotmail.com>
2025-03-01 23:14:41 -0500

    ok

--- backend_exp/exp_server.py
+++ backend_exp/exp_server.py
@@ -5,16 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def experiment():
-    app = Flask(__name__)
-
-    @app.route('/')
-    def experiment():
-        response = jsonify({"error": "Experimental Server encountered an issue"})
-        response.status_code = 500
-        return response
-
-    if __name__ == '__main__':
-        app.run(host='198.162.0.1', port=0)
+    response = jsonify({"error": "Experimental Server encountered an issue"})
+    response.status_code = 500
+    return response
 
 if __name__ == '__main__':
     app.run(host='198.162.0.1', port=0)
